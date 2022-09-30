a1ec4f3b750e3e74577719761bfde4989a7274e2
Bruno Leite <brunoleite@Brunos-MacBook-Pro.local>
2022-09-30 07:24:15 -0300

    commit inicial agencia micro cliente

---
+++ agencia-micro-cliente/src/main/java/org/brunofsleite/cliente/Cliente.java
@@ -0,0 +1,11 @@
+package org.brunofsleite.cliente;
+
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Cliente extends PanacheEntity {
+    
+    public String nome;
+}
